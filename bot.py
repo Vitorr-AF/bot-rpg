528985e4baf41f3c9d20b62b5e22948a0916e46b
Vitorr-AF <vitoraugustofranca7@gmail.com>
2024-05-28 17:17:54 -0300

    Projeto criado (ainda não funcional)

---
+++ bot.py
@@ -0,0 +1,4 @@
+from teste import *
+
+x = ler_ficha('registro')
+print(x)
---
+++ teste.py
@@ -0,0 +1,34 @@
+def ler_ficha(nome):
+    with open(f'{nome}.txt', 'r', encoding='utf-8') as ficha_lista:
+        ficha = ficha_lista.read()
+
+        return ficha
+
+
+def ver_mod(valor):
+    try:
+        mod = int((valor - 10) // 2)
+        if mod >= 0:
+            return f'+{mod}'
+        else:
+            return f'{mod}'
+    except ValueError:
+        print('Valor de habilidade inváido')
+        return '???'
+    except TypeError:
+        print('Valor de habilidade inváido')
+        return '???'
+
+
+
+# def ler_ficha(nome):
+#     with open(f'{nome}.txt', 'r', encoding='utf-8') as ficha_lista:
+#         linhas = ficha_lista.readlines()
+#         ficha = ""
+#         for line in linhas:
+#             ficha = ficha + line
+
+
+
+
+#         return ficha
