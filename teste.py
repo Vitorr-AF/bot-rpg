585e4847adae88982abd1c74bda862606763252a
Vitorr-AF <vitoraugustofranca7@gmail.com>
2024-05-29 11:38:24 -0300

    Adicionada função de encontrar ficha e comentários

--- teste.py
+++ teste.py
@@ -1,14 +1,14 @@
 import random
 
-
-def ler_ficha(nome):
+# Retorna a ficha completa de um personagem como uma string
+def ler_ficha(nome): # terminado
     with open(f'{nome}.txt', 'r', encoding='utf-8') as ficha_lista:
         ficha = ficha_lista.read()
 
         return ficha
 
-
-def ver_mod(valor):
+# Retorna o modificador de um valor de habilidade
+def ver_mod(valor): # terminado
     try:
         mod = (valor - 10) // 2
         if mod >= 0:
@@ -22,18 +22,19 @@ def ver_mod(valor):
         print('Valor de habilidade inváido')
         return '???'
 
-
-def dado(lados):
+# Rola um dado com o número de lados
+def dado(lados): # terminado
     resultado = random.randint(1, lados)
     return resultado
 
-
-def ler_token():
+# Lê o token do bot
+def ler_token():# terminado
     with open('token.txt', 'r', encoding='utf-8') as token:
         return token.read()
 
-
-def conferir_registro(player):
+# Confere se um jogador está registrado ou não pelo id 
+# Index[0] tem o nome da ficha, e index[1] tem o valor True ou False pra registrado ou não
+def conferir_registro(player): # terminado
     with open('registrado.txt', 'r', encoding='utf-8') as registros:
         linhas = registros.readlines()
         for x in linhas:
@@ -42,8 +43,8 @@ def conferir_registro(player):
                 return linha[1], True
         return 'Jogador não registrado', False
 
-
-def ver_mod_player(player, habilidade):
+# Retorna o modificador de uma habilidade específica de um player específico pelo id
+def ver_mod_player(player, habilidade): # terminado
     ficha = encontrar_ficha(player)
     for x in ficha:
         linha = x.split(' ')
@@ -52,16 +53,9 @@ def ver_mod_player(player, habilidade):
             return mod
     print('Habilidade inválida!')
 
-
-
-# def ler_ficha(nome):
-#     with open(f'{nome}.txt', 'r', encoding='utf-8') as ficha_lista:
-#         linhas = ficha_lista.readlines()
-#         ficha = ""
-#         for line in linhas:
-#             ficha = ficha + line
-
-
-
-
-#         return ficha
+# Retorna a ficha completa de um personagem como uma lista
+def encontrar_ficha(player): # terminado
+    arquivo, _ = conferir_registro(player)
+    with open(f'{arquivo}.txt', 'r', encoding='utf-8') as ficha_lista:
+        ficha = ficha_lista.readlines()
+    return ficha
