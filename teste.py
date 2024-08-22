b0dd53c0b73ede7d0f071a47b2a89dd78559e871
Vitorr-AF <vitoraugustofranca7@gmail.com>
2024-08-22 09:27:18 -0300

    Descrições de funções adicionadas

--- teste.py
+++ teste.py
@@ -1,14 +1,20 @@
 import random
 
-# Retorna a ficha completa de um personagem como uma string
+
 def ler_ficha(nome): # terminado
+    """
+    Retorna a ficha completa de um personagem como uma string
+    """
     with open(f'{nome}.txt', 'r', encoding='utf-8') as ficha_lista:
         ficha = ficha_lista.read()
 
         return ficha
 
-# Retorna o modificador de um valor de habilidade
+
 def ver_mod(valor): # terminado
+    """
+    Retorna o modificador de um valor de habilidade
+    """
     print(valor)
     valor = int(valor)
     try:
@@ -21,19 +27,27 @@ def ver_mod(valor): # terminado
         print('Valor de habilidade inváido')
         return '???'
 
-# Rola um dado com o número de lados
-def dado(lados): # terminado
+
+def dado(lados=20): # terminado
+    """
+    Rola um dado com o número de lados fornecido (padrão 20)
+    """
     resultado = random.randint(1, lados)
     return resultado
 
-# Lê o token do bot
+
 def ler_token():# terminado
+    """
+    Lê o token do bot
+    """
     with open('token.txt', 'r', encoding='utf-8') as token:
         return token.read()
 
-# Confere se um jogador está registrado ou não pelo id 
-# Index[0] tem o nome da ficha, e index[1] tem o valor True ou False pra registrado ou não
+
 def conferir_registro(player): # terminado
+    """
+    Confere se um jogador está registrado ou não pelo seu id do discord, retorna o nome da ficha e o valor True ou false pra registrado ou não
+    """
     with open('registrado.txt', 'r', encoding='utf-8') as registros:
         linhas = registros.readlines()
         for x in linhas:
@@ -42,8 +56,11 @@ def conferir_registro(player): # terminado
                 return linha[1].strip(), True
         return 'Jogador não registrado', False
 
-# Retorna o modificador de uma habilidade específica de um player específico pelo id
+
 def ver_mod_player(player, habilidade): # terminado
+    """
+    Retorna o modificador de uma habilidade específica de um player pelo id do discord
+    """
     ficha = encontrar_ficha(player)
     for x in ficha:
         linha = x.split(' ')
@@ -52,15 +69,21 @@ def ver_mod_player(player, habilidade): # terminado
             return int(mod)
     print('Habilidade inválida!')
 
-# Retorna a ficha completa de um personagem como uma lista
+
 def encontrar_ficha(player): # terminado
+    """
+    Retorna a ficha completa de um personagem como uma lista
+    """
     arquivo, _ = conferir_registro(player)
     with open(f'{arquivo}.txt', 'r', encoding='utf-8') as ficha_lista:
         ficha = ficha_lista.readlines()
     return ficha
 
-# Registra a ficha de um player pelo id
+
 def registrar_player(player):
+    """
+    Registra a ficha de um player pelo id do discord
+    """
     with open('registrado.txt', 'r', encoding='utf-8') as registros:
         linhas = registros.readlines()
     num_player = len(linhas) + 1
@@ -70,8 +93,11 @@ def registrar_player(player):
         ficha.write(f"Nome: player{num_player}\nFor: 10\nDes: 10\nCon: 10\nInt: 10\nSab: 10\nCar: 10")
     atualizar_gitignore(f'player{num_player}.txt')
 
-# Muda o valor de uma habilidade
+
 def registrar_habilidade(player, habilidade, valor):
+    """
+    Muda o valor de uma habilidade
+    """
     ficha = encontrar_ficha(player)
     for x in range(len(ficha)):
         linha = ficha[x].split(' ')
@@ -89,8 +115,11 @@ def registrar_habilidade(player, habilidade, valor):
     reescrever_ficha(player, ficha)
     return
 
-# Pega uma lista com os novos valores da lista e substitui ela
+
 def reescrever_ficha(player, ficha):
+    """
+    Pega uma lista com os novos valores da ficha e substitui ela
+    """
     arquivo, _ = conferir_registro(player)
     ficha_nova = ""
     for linha in ficha:
@@ -100,6 +129,9 @@ def reescrever_ficha(player, ficha):
 
 
 def atualizar_gitignore(player_filename):
+    """
+    Atualiza o gitignore pra ignorar um arquivo
+    """
     gitignore_path = '.gitignore'
     
     try:
@@ -114,5 +146,8 @@ def atualizar_gitignore(player_filename):
 
 
 def checar_mestre(cargos):
+    """
+    Confere se alguem tem o cargo de DM
+    """
     mestre = any(role.name == "DM" for role in cargos)
     return mestre
